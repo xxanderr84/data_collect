2b48a8de23dbc80e4cffb1e5beed80e520ea69e6
xxanderr84 <xxanderr84@gmail.com>
2020-10-16 13:48:59 +0500

    completed

--- homework/lesson_02/task.py
+++ homework/lesson_02/task.py
@@ -5,7 +5,7 @@ import json
 
 def get_salary(input_string: str):
     output_dict = {}
-    if len(input_string) == 0:
+    if len(input_string) == 0 or input_string == 'По договорённости':
         output_dict['currency'] = None
         output_dict['min'] = None
         output_dict['max'] = None
@@ -37,13 +37,11 @@ def get_salary(input_string: str):
 
 
 # https://hh.ru/search/vacancy?L_is_autosearch=false&clusters=true&enable_snippets=true
-i = 0
-j = 0
 main_link = 'https://hh.ru/'
 params = {'L_is_autosearch': 'false',
           'clusters': 'true',
           'enable_snippets': 'true',
-          'text': 'Инженер',
+          'text': 'python',
           'page': '0'}
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/86.0.4240.75 Safari/537.36'}
@@ -51,10 +49,9 @@ vacancies = []
 while True:
     response = requests.get(main_link + 'search/vacancy', params=params, headers=headers)
     soup = bs(response.text, 'html.parser')
-    next_page = soup.find('a', {'data-qa': 'pager-next'})
     vacancies_list = soup.findAll('div', {'data-qa': 'vacancy-serp__vacancy'})
     for vacancy in vacancies_list:
-        vacancy_data = {}
+        vacancy_data = {'site': main_link}
         vacancy_title = vacancy.find('a', {'data-qa': 'vacancy-serp__vacancy-title'})
         vacancy_data['title'] = vacancy_title.text
         vacancy_data['url'] = vacancy_title['href']
@@ -63,13 +60,34 @@ while True:
             vacancy_data.update(get_salary(''))
         else:
             vacancy_data.update(get_salary(vacancy_salary.text))
-        vacancy_data['site'] = main_link
         vacancies.append(vacancy_data)
+    next_page = soup.find('a', {'data-qa': 'pager-next'})
     if next_page is None:
         break
     params['page'] = next_page['data-page']
+
+# https://www.superjob.ru/vacancy/search/?keywords=python&noGeo=1&page=2
+main_link = 'https://www.superjob.ru/'
+add_link = 'vacancy/search/?keywords=python&noGeo=1'
+while True:
+    response = requests.get(main_link + add_link, headers=headers)
+    soup = bs(response.text, 'html.parser')
+    vacancies_list = soup.findAll('div', {'class': 'iJCa5 f-test-vacancy-item _1fma_ undefined _2nteL'})
+    for vacancy in vacancies_list:
+        vacancy_data = {'site': main_link}
+        vacancy_info = vacancy.find('a')
+        vacancy_data['title'] = vacancy_info.text
+        vacancy_data['url'] = vacancy_info['href']
+        vacancy_salary = vacancy.find('span', {'class': '_3mfro _2Wp8I PlM3e _2JVkc _2VHxz'})
+        if vacancy_salary is None:
+            vacancy_data.update(get_salary(''))
+        else:
+            vacancy_data.update(get_salary(vacancy_salary.text))
+        vacancies.append(vacancy_data)
+    next_page = soup.find('a', {'rel': 'next'})
+    if next_page is None:
+        break
+    add_link = next_page['href']
+
 with open('response.json', 'w') as f:
-    json.dump(vacancies, f, ensure_ascii=False)
-# data-qa vacancy-serp__vacancy-title  - наименование вакансии
-# vacancy-serp__vacancy-compensation  - зарплата
-# vacancy-serp__vacancy vacancy-serp__vacancy_premium
+    json.dump(vacancies, f)
